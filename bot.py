e390742f1af476da7cb50c936dcbcf155ad543e5
BNKA1993 <bnka1993@gmail.com>
2024-03-27 15:53:57 +0000

    add_current_date

--- bot.py
+++ bot.py
@@ -4,9 +4,11 @@ import telebot  # импорт библиотеки для работы с Teleg
 from telebot import types  # импорт модуля из библиотеки Telebot, для фич интерфейса
 import random  # импорт модуля для выбора рандомных значений
 import logging  # импорт модуля для системы логирования
+import datetime
 import re
 import nltk
 from nltk.tokenize import word_tokenize
+from PIL import Image
 # возможность загрузки переменных среды из файла .env
 from dotenv import load_dotenv
 load_dotenv()
@@ -19,6 +21,7 @@ logger = logging.getLogger(__name__)  # подключение системы л
 
 bot = telebot.TeleBot(token)
 
+# список пожеланий для пользователя, которые отправляются рандомно
 random_wishes = [
     "Привет! Желаю тебе замечательного дня!",
     "Привет! Пусть у тебя все будет прекрасно!",
@@ -28,7 +31,9 @@ random_wishes = [
     "Привет! Зарядись позитивом и силами на весь день!",
     "Привет! Желаю тебе море позитивных эмоций и успехов!",
     "Привет! Пусть этот день принесет тебе много радости!"
-]  # список пожеланий для пользователя, которые отправляются рандомно
+]  
+
+# список ответов для пользователя, которые отправляются рандомно
 random_responses = [
     "Я не знаю, что на это ответить.",
     "Извини, я не понимаю.",
@@ -37,8 +42,7 @@ random_responses = [
     "Пожалуйста, уточните ваш вопрос.",
     "Мне трудно дать точный ответ на это.",
     "Простите, я не могу ответить на это.",
-]  # список ответов для пользователя, которые отправляются рандомно
-
+]
 
 # определение функции обработки команды /start от пользователя
 @bot.message_handler(commands=['start'])
@@ -58,41 +62,48 @@ def start_message(message):
     itembtn_hello = types.KeyboardButton('Пожелания на день')
     # создание кнопки "Как дела?" для обычной клавиатуры
     itembtn_how_are_you = types.KeyboardButton('Как дела?')
+    # Добавление кнопки "Сегодняшнее число"
+    today_button = types.KeyboardButton('Сегодняшнее число')
     # добавление созданных кнопок в объект reply_markup
-    reply_markup.add(itembtn_hello, itembtn_how_are_you)
-# отправка сообщения пользователю с приветственным сообщением и встроенной клавиатурой
+    reply_markup.add(itembtn_hello, itembtn_how_are_you, today_button)
+
+
+    # отправка сообщения пользователю с приветственным сообщением и встроенной клавиатурой
     bot.send_message(
         message.chat.id, 'Хай! Для начала давай я расскажу, что умею.', reply_markup=inline_markup)
-# отправка еще одного сообщение с текстом "Выбери вариант:" и обычной клавиатурой
+    # отправка еще одного сообщение с текстом "Выбери вариант:" и обычной клавиатурой
     bot.send_message(message.chat.id, 'Выбери вариант:',
                      reply_markup=reply_markup)
-# запись информации о том, что пользователь запустил бота в лог
+    # запись информации о том, что пользователь запустил бота в лог
+
     logger.info(f"Пользователь {message.from_user.username} запустил бота.")
 
+    
+
 # Определяем функцию-обработчик callback-запросов
-
-
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
     # Проверяем значение поля data в callback-запросе
     if call.data == 'yes':
         # Если значение равно 'yes', отправляем сообщение пользователю с подтверждением
         bot.send_message(
-            call.message.chat.id, 'Я могу здороваться, спрашивать и отвечать на вопрос "Как дела?" и желать тебе хорошего дня!')
+            call.message.chat.id, 'Я умею спрашивать и отвечать на вопрос "Как дела?"\nПоказывать текущую дату\nОтправлять пожелания на день')
     # Если значение равно 'no', отправляем сообщение пользователю с сожалением
     elif call.data == 'no':
         bot.send_message(call.message.chat.id,
                          'Жаль, что ты не хочешь меня узнать:(')
 
+
 # Определяем функцию-обработчик текстовых сообщений
+
 @bot.message_handler(content_types=['text'])
 def handle_h1(message):
     # Записываем информацию о полученном сообщении в лог
     logger.info(f"Получено сообщение от пользователя {
                 message.from_user.username}: {message.text}")
-     # Проверяем текст сообщения
+    # Проверяем текст сообщения
     if message.text == "Привет":
-         # Если сообщение "Привет", отправляем случайное пожелание
+        # Если сообщение "Привет", отправляем случайное пожелание
         send_random_wish(message.chat.id)
     elif message.text == "Пожелания на день":
         # Если сообщение "Пожелания на день", также отправляем случайное пожелание
@@ -101,6 +112,8 @@ def handle_h1(message):
         # Если сообщение "Как дела?", отправляем ответ о состоянии бота
         bot.send_message(
             message.chat.id, "У меня все отлично, спасибо! Как у тебя?")
+    elif message.text == "Сегодняшнее число":
+        send_today_message(message.chat.id)
     elif message.text in ["Хорошо", "хорошо", "отлично", "Отлично", "Супер", "супер"]:
         # Если сообщение соответствует одному из вариантов хорошего состояния, отправляем поздравление
         bot.send_message(message.chat.id, "Я рад за тебя!" + "\U0001F60A")
@@ -111,7 +124,16 @@ def handle_h1(message):
         # Если сообщение не соответствует ни одному из вышеуказанных, отправляем случайный ответ
         send_random_responses(message.chat.id)
 
+
+def send_today_message(chat_id):
+    # Получаем текущую дату
+    today_date = datetime.date.today()
+    # Отправляем сообщение с текущей датой
+    bot.send_message(chat_id, f"Сегодня {today_date}")
+
+
 # Определяем функцию для отправки случайного пожелания
+
 def send_random_wish(chat_id):
     # Выбираем случайное пожелание из списка
     random_wish = random.choice(random_wishes)
@@ -120,7 +142,9 @@ def send_random_wish(chat_id):
     # Записываем информацию о пожелании в лог
     logger.info(f"Отправлено пожелание пользователю с ID {chat_id}.")
 
+
 # Определяем функцию для отправки случайного ответа
+
 def send_random_responses(chat_id):
     # Выбираем случайный ответ из списка
     random_response = random.choice(random_responses)
@@ -130,7 +154,13 @@ def send_random_responses(chat_id):
     logger.info(f"Отправлен случайный ответ пользователю с ID {chat_id}.")
 
 
-# chat_id = "236333709"  # Замените на реальный идентификатор чата пользователя
-# bot.send_message(chat_id, "Бот тоже хочет внимания)))")
-# logger.info(f"Отправлено сообщение пользователю А еще я так могу \U0001F60A с ID {chat_id}.")
-bot.polling(none_stop=True, interval=0)
+try:
+    bot.polling(none_stop=True, interval=0)
+except Exception as e:
+    print(f"Ошибка при запуске бота: {e}")
+
+
+# # chat_id = "236333709"  # Замените на реальный идентификатор чата пользователя
+# # bot.send_message(chat_id, "Бот тоже хочет внимания)))")
+# # logger.info(f"Отправлено сообщение пользователю А еще я так могу \U0001F60A с ID {chat_id}.")
+# bot.polling(none_stop=True, interval=0)
